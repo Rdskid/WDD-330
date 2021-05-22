c3bfec2f9819e99822e942abd003112588fa46c6
rdskid <dskidmorelds@gmail.com>
2021-05-21 19:41:48 -0600

    final week 5 push

--- week5/js/hikes.js
+++ week5/js/hikes.js
@@ -1,72 +1,90 @@
-
-const hikeList = [
-  {
-    name: "Bechler Falls",
-    imgSrc: "../week5/images/waterfall.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "3 miles",
-    difficulty: "Easy",
-    description:
-      "Beautiful short hike along the Bechler river to Bechler Falls",
-    directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road.Drive to the end of the Cave Falls road. There is a parking area at the trailhead."
-  },
-  {
-    name: "Teton Canyon",
-    imgSrc: "../week5/images/waterfall.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "3 miles",
-    difficulty: "Easy",
-    description: "Beautiful short (or long) hike through Teton Canyon.",
-    directions:
-      "Take Highway 33 East to Driggs. Turn left onto Teton Canyon Road. Follow that road for a few miles then turn right onto Stateline Road for a short distance, then left onto Alta Road. Veer right after Alta back onto Teton Canyon Road. There is a parking area at the trailhead."
-  },
-  {
-    name: "Denanda Falls",
-    imgSrc: "../week5/images/waterfall.jpg",
-    imgAlt: "Image of Bechler Falls",
-    distance: "7 miles",
-    difficulty: "Moderate",
-    description:
-      "Beautiful hike through Bechler meadows river to Denanda Falls",
-    directions:
-      "Take Highway 20 north to Ashton. Turn right into the town and continue through. Follow that road for a few miles then turn left again onto the Cave Falls road. Drive to until you see the sign for Bechler Meadows on the left. Turn there. There is a parking area at the trailhead."
+export default class Hikes {
+  constructor() {
+    this.imgBasePath = "//byui-cit.github.io/cit261/examples/";
   }
-]
-
-//loops information to show cards
-function createHTML() {
-
-
-for (let i = 0; i < hikeList.length; i++ ) {
-  
-  let hikeCard = document.createElement('section');
-  let hikeName = document.createElement('h2');
-  let image = document.createElement('img');
-  let distance = document.createElement('h3');
-  let difficulty = document.createElement('h3');
-  let description = document.createElement('p');
-  let direction = document.createElement('p');
-
-  hikeName.textContent = hikeList[i].name;
-  distance.textContent = hikeList[i].distance;
-  difficulty.textContent = hikeList[i].difficulty;
-  description.textContent = hikeList[i].description;
-  direction.textContent = hikeList[i].directions;
-
-  image.setAttribute('src', hikeList[i].imgSrc);
-  image.setAttribute('alt', hikeList[i].imgAlt);
-
-  hikeCard.appendChild(hikeName);
-  hikeCard.appendChild(distance);
-  hikeCard.appendChild(difficulty);
-  hikeCard.appendChild(description);
-  hikeCard.appendChild(direction);
-  hikeCard.appendChild(image);
-
-  document.querySelector('div.hikes').appendChild(hikeCard);
+  showHikeList(hikeList, hikeListElement) {
+    hikeListElement.innerHTML = "";
+    this.renderHikeList(hikeList, hikeListElement);
+  }
+  renderHikeList(hikes, parent) {
+    hikes.forEach((hike) => {
+      parent.appendChild(this.renderOneHike(hike));
+    });
+    this.setImageEventListener(hikes);
+  }
+  renderOneHike(hike) {
+    const item = document.createElement("li");
+    item.setAttribute("id", hike.id);
+    item.innerHTML = ` <h2>${hike.name}</h2>
+          <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+          <div>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+          </div>`;
+    return item;
+  }
+  setImageEventListener(hikes) {
+    hikes.forEach((hike) => {
+      //document.getElementById("mybtn").addEventListener("click", () {
+      document.getElementById(hike.id).addEventListener("click", (event) => {
+        event.preventDefault();
+        const fullDetails = document.getElementById(hike.id);
+        fullDetails.innerHTML = ` <h2>${hike.name}</h2>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+                  <h3>Description</h3>
+              <p>${hike.description}</p><br>
+           <h3>Directions</h3>
+               <p>${hike.directions}</p><br> 
+               <button id="mybtn${hike.id}">Go Back</button>
+              `;
+        document
+          .getElementById("mybtn" + hike.id)
+          .addEventListener("click", (event) => {
+            event.stopPropagation();
+            const backDetails = document.getElementById(hike.id);
+            backDetails.innerHTML = ` <h2>${hike.name}</h2>
+            <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+            <div>
+                    <div>
+                        <h3>Distance</h3>
+                        <p>${hike.distance}</p>
+                    </div>
+                    <div>
+                        <h3>Difficulty</h3>
+                        <p>${hike.difficulty}</p>
+                    </div>
+            </div>`;
+          });
+      });
+    });
+  }
+  renderFullDetails(hike) {
+    const fullDetails = document.getElementById("click");
+    fullDetails.innerHTML = ` <h2>${hike.name}</h2>
+          <div class="image"><img src="${this.imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}" id=${hike.id}></div>
+          <div>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+          </div>`;
+    fullDetails.classList.remove("nodisplay");
   }
 }
-export default {
-  createHTML
-};
--- week5/js/script.js
+++ week5/js/script.js
@@ -1,36 +1,21 @@
-const name = document.querySelector('#name')
-const password = document.querySelector('#password')
-const passwordConfirm = document.querySelector('#confirmPassword')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+class MultiplicatorUnitFailure extends Error {}
 
-form.addEventListener('submit', (e) => {
-  let messages = []
-  if (name.value === '' || name.value == null) {
-    messages.push('Name is required')
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
   }
+}
 
-  if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
+function reliableMultiply(a, b) {
+  for (;;) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure)) throw e;
+    }
   }
+}
 
-  if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
-  }
-
-  if (password.value === 'password' ) {
-    messages.push('Password cannot be password')
-  }
-  if (password.value === '' || password.value == null) {
-      messages.push('Password must not be blank or null')
-  }
-
-  if (password.value != passwordConfirm.value) {
-      messages.push('Password does not match confirmation')
-  }
-
-  if (messages.length > 0) {
-    e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-  }
-})
+console.log(reliableMultiply(8, 8));
